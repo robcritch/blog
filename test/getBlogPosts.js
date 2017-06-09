eb58f7bffaffc4def6db5ea05b1ba16d9e9ba591
Rob Critch <rob.critch@beautybay.com>
2017-06-09 22:07:31 +0100

    When a blog post is added, the blog post is returned

--- test/getBlogPosts.js
+++ test/getBlogPosts.js
@@ -2,23 +2,64 @@ const assert = require('assert')
 
 describe('Given no blog posts existing', () => {
 	it('returns no blog posts', (done) => {
-		const blogPosts = new BlogPosts()
+		const blogPostDataSource = new FakeBlogPostDataSource()
+
+		const blogPosts = new BlogPosts(blogPostDataSource)
 
 		blogPosts.retrievePosts()
 		.then(result => {
-			assert.deepEqual(result, {})
+			assert.deepEqual(result, [])
 			done()
 		})
 		.catch(err => done(err))
 	})
 })
 
-class BlogPosts {
+describe('Given a blog post existing', () => {
+	it('returns the blog post', (done) => {
+		const blogPostDataSource = new FakeBlogPostDataSource()
+		
+		blogPostDataSource
+		.withBlogPost('new blog post')
+		const blogPosts = new BlogPosts(blogPostDataSource)
+
+		blogPosts.retrievePosts()
+		.then(result => {
+			assert.deepEqual(result, ['new blog post'])
+			done()
+		})
+		.catch(err => done(err))
+	})
+})
+
+class FakeBlogPostDataSource {
 	constructor() {
-		this.blogPosts = {}
+		this.blogPosts = []
+	}
+
+	subscribe(onBlogPostReceived) {
+		this.blogPosts.forEach(blogPost => onBlogPostReceived(blogPost))
+	}
+
+	withBlogPost(blogPost) {
+		this.blogPosts.push(blogPost)
+		return this
+	}
+
+}
+
+class BlogPosts {
+	constructor(blogPostDataSource) {
+		this.blogPosts = []
+		this.blogPostDataSource = blogPostDataSource
+		this.blogPostDataSource.subscribe(this.addBlogPost.bind(this))
 	}
 
 	retrievePosts() {
 		return Promise.resolve(this.blogPosts)
+	}
+
+	addBlogPost(blogPost) {
+		this.blogPosts.push(blogPost)
 	}
 }
