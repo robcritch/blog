d3dedbfc1938f800d658b18eb704b4d31075afeb
Rob Critch <rob.critch@beautybay.com>
2017-06-20 15:28:39 +0100

    Reads out from elastic, listening for updates

--- src/blogPosts.js
+++ src/blogPosts.js
@@ -1,6 +1,6 @@
 class BlogPosts {
 	constructor(blogPostDataSource) {
-		this.blogPosts = []
+		this.blogPosts = {}
 		this.blogPostDataSource = blogPostDataSource
 		this.blogPostDataSource.subscribe(this.addBlogPost.bind(this))
 	}
@@ -10,8 +10,20 @@ class BlogPosts {
 	}
 
 	addBlogPost(blogPost) {
-		this.blogPosts.push(blogPost)
+		const { author, date, title, content} = blogPost
+		this.blogPosts[blogPost.title] = new BlogPost(author, date, title, content)
+		//this.blogPosts.push(blogPost)
+		console.log(this.blogPosts)
 	}
 }
 
 module.exports = BlogPosts
+
+class BlogPost {
+	constructor(author, date, title, content) {
+		this.author = author
+		this.date = date
+		this.title = title
+		this.content = content
+	}
+}
--- src/elasticBlogSource.js
+++ src/elasticBlogSource.js
@@ -2,8 +2,10 @@ const elasticsearch = require('elasticsearch')
 
 class ElasticBlogSource {
 	constructor() {
-		this.client = new elasticsearch.Client({host: 'localhost:9200'})
+		this.client = new elasticsearch.Client({host: 'elastic:changeme@localhost:9200'})
 		this.onNewBlogPosts = () => {}
+		this.getBlogPosts()
+    	.then(() => setInterval(() => this.getBlogPosts(), 5000))
 	}
 
 	subscribe(callback) {
@@ -17,10 +19,15 @@ class ElasticBlogSource {
 			size: 50
 		})
 		.then(({ hits }) => {
-			return hits.hits.map(({ _source }) =>  {
-				this.onNewBlogPosts = _source
-			})
-		} )
+			return hits.hits.map(({ _source }) =>
+				this.onNewBlogPosts({
+					author: _source.author,
+					date: _source.post_date,
+					title: _source.post_title,
+					content: _source.post_content
+				})
+			)
+		})
 	}
 }
 
